9d060b390883ff01242d10b33c46f06b0803f6fe
Steve Hackbarth <stephenhackbarth@gmail.com>
2025-05-28 10:57:34 -0400

    allow users to update the system prompt

---
+++ nextjs-app/pages/api/compass/save-system-prompt.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs/promises";
+
+type ResponseData = {};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  try {
+    console.log(req.body)
+    const filePath = "../prompts/interrogation.txt";
+    const fileContent = await fs.writeFile(filePath, req.body.systemPrompt, "utf-8");
+    res.status(200).end();
+  } catch (err) {
+    console.log(err);
+    res.status(500).end();
+  }
+}
+
